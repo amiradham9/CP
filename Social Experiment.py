3ad7d2de4200017ab6906b53e9bcbf563eca1d7a
amiradham9 <amiradham7910@gmail.com>
2026-02-26 11:29:55 +0300

    Create Social Experiment.py

---
+++ Social Experiment.py
@@ -0,0 +1,13 @@
+n=int(input())
+for _ in range (n):
+    m=int(input())
+    if m%2==0:
+        if m==2:
+            print(2)
+        else:
+            print(0)
+    else:
+        if m==3:
+            print(3)
+        else:
+            print(1)
